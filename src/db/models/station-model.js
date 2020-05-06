95d57a4451e157a7b73274ed9a8e62e5d0e7af6a
quijano <safiro.quijano@nokia.com>
2020-05-06 14:58:40 +0800

    installed jwt and used in station model for authorization

--- src/db/models/station-model.js
+++ src/db/models/station-model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const stationTypes = ['service, order, seats'];
-
+const SECRET_KEY = process.env.CCS_SECRET || 'defaultsecretkey';
 
 /* STATION (agents)
 *   Description: stations are nodes 
@@ -37,6 +38,15 @@ const stationSchema = new mongoose.Schema({
         }
     }]
 });
+
+//Station model method for generating JWT authorization tokens
+stationSchema.methods.generateToken = async function(){
+    const station = this;
+    const token = await jwt.sign({_id: station.id.toString()}, SECRET_KEY);
+    if(token) station.token = token;
+    await station.save();
+    return token;
+}
 
 const Station = mongoose.model('Station',stationSchema);
 module.exports = Station;
