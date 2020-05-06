834e54145355a2f6ecf01b55a9676ae73abc3eb9
quijano <safiro.quijano@nokia.com>
2020-05-06 18:25:36 +0800

    created router for stations

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 require('./db/mongoose');
 const nexusRouter = require('./routers/nexus-router');
+const stationRouter = require('./routers/station-router');
 const PORT  = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(nexusRouter);
-
+app.use(stationRouter);
 
 app.listen(PORT,()=>{
     console.log(`listening on port: ${PORT}`);
---
+++ src/routers/station-router.js
@@ -0,0 +1,7 @@
+const express = require ('express');
+const Station = require('../db/models/station-model');
+const router = new express.Router();
+
+
+
+module.exports = router;
